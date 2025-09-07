da9f98dd9778200bf3dbaff5855f0b28767772af
Vitny <thevitny@gmail.com>
2025-09-07 21:43:15 +0300

    init: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (matrix === undefined) return [];
+  return matrix
+    .map((subArr, i) => (i % 2 !== 0 ? subArr.reverse() : subArr))
+    .flat();
 };
